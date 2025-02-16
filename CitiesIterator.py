fbcf457be522e2fadeea366c23d5f62e39d72caa
Kirill863 <Byblik4310@gmail.com>
2025-02-16 18:15:13 +0300

    добавил функцию иницилизации

--- CitiesIterator.py
+++ CitiesIterator.py
@@ -10,4 +10,11 @@ class City:
     subject: str
 
 class CitiesIterator:
-    
+    def __init__(self, cities_data: List[Dict], sort_by: str = None, reverse: bool = False):
+        
+        self.cities_data = cities_data
+        self._validate_data()
+        self.cities = [self._create_city(city_data) for city_data in cities_data]
+        if sort_by is not None:
+            self.sort_by(sort_by, reverse)
+        self.min_population = 0  
